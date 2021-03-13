74eefdb4ef7f9931ffb2ad02c2e58b61edfedc58
Riade Benbaki <riade.benbaki@allemagne.polytechnique.fr>
2021-03-13 10:42:41 +0100

    Added revised version

--- src/apm_ompic.c
+++ src/apm_ompic.c
@@ -12,7 +12,8 @@
  #include <mpi.h>
  #include <omp.h>
 
-
+ // MACRO for calculating the time
+ #define DIFFTEMPS(a,b) (((b).tv_sec - (a).tv_sec) + ((b).tv_usec - (a).tv_usec)/1000000.)
  #define APM_DEBUG 0
  
  char * 
@@ -115,232 +116,219 @@ char* cuda_malloc_cp(char *buf, int size);
 int* cuda_malloc(int size);
 int  finalcudaCall(char* cpattern,char * cbuf, int cuda_end, int * results_th , int  nth_b,int nblock);
 void kernelCall(char* cpattern,char * cbuf,int cuda_end ,int n_bytes, int size_pattern, int approx_factor, int * results_th , int  nth_b,int nblock);
+void AssignDevices(int rank);
+void cuda_free( void *buf );
 
- int main( int argc, char ** argv )
- {
-   char ** pattern ;
-   char * filename ;
-   char * local_buf;
-   int local_buf_size;
-   int buf_size; //buf size for each process without shadow cells (a part from rank 0)
-   int max_pat = 0; // size of the largest Pattern
-   int approx_factor = 0 ;
-   int nb_patterns = 0 ;
-   int i,j ;
-   struct timeval t1, t2;
-   double duration ;
-   int n_bytes ;
-   int * n_matches, *glob_matches ;
-   int rank, size;
-   MPI_Init (&argc, &argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);	/* who am i */  
-   MPI_Comm_size(MPI_COMM_WORLD, &size); /* number of processes */ 
+int main( int argc, char ** argv ){
+  char ** pattern ;
+  char * filename ;
+  char * local_buf;
+  int local_buf_size;
+  int buf_size; //buf size for each process without shadow cells (a part from rank 0)
+  int max_pat = 0; // size of the largest Pattern
+  int approx_factor = 0 ;
+  int nb_patterns = 0 ;
+  int i,j ;
+  struct timeval t0, t1, t2;
+  int n_bytes ;
+  int * n_matches, *glob_matches ;
+  int rank, size;
+
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);	/* who am i */  
+  MPI_Comm_size(MPI_COMM_WORLD, &size); /* number of processes */ 
    
    /* Check number of arguments */
-   if ( argc < 4 ) 
-   {
-     if(rank == 0 )
-       printf( "Usage: %s approximation_factor "
-               "dna_database pattern1 pattern2 ...\n", 
-               argv[0] ) ;
-     MPI_Finalize();
-     return 1 ;
+  if ( argc < 4 ){
+    if(rank == 0 )
+      printf( "Usage: %s approximation_factor "
+              "dna_database pattern1 pattern2 ...\n", 
+              argv[0] ) ;
+    MPI_Finalize();
+    return 1 ;
    }
-   /* Get the distance factor */
-   approx_factor = atoi( argv[1] ) ;
- 
+
+  /* Get the distance factor */
+  approx_factor = atoi( argv[1] ) ;
+
    /* Grab the filename containing the target text */
-   filename = argv[2] ;  
- 
+  filename = argv[2] ;  
+
    /* Get the number of patterns that the user wants to search for */
-   nb_patterns = argc - 3 ;
- 
+  nb_patterns = argc - 3 ;
+
    /* Fill the pattern array */
-   pattern = (char **)malloc( nb_patterns * sizeof( char * ) ) ;
-   if ( pattern == NULL ) 
-   {
-       fprintf( stderr, 
-               "Unable to allocate array of pattern of size %d\n", 
-               nb_patterns ) ;
-       return 1 ;
-   }
+  pattern = (char **)malloc( nb_patterns * sizeof( char * ) ) ;
+  if ( pattern == NULL ){
+    fprintf(stderr, 
+            "Unable to allocate array of pattern of size %d\n", 
+            nb_patterns ) ;
+    return 1 ;
+  }
  
    /* Grab the patterns */
-   for ( i = 0 ; i < nb_patterns ; i++ ) 
-   {
-       int l ;
-       l = strlen(argv[i+3]) ;
- 
-       if( l > max_pat )
-         max_pat = l;
+  for ( i = 0 ; i < nb_patterns ; i++ ){
+      int l = strlen(argv[i+3]) ;
+      
+      if( l > max_pat )
+        max_pat = l;
        
-       if ( l <= 0 ) 
-       {
-           fprintf( stderr, "Error while parsing argument %d\n", i+3 ) ;
-           return 1 ;
-       }
+      if ( l <= 0 ){
+        fprintf( stderr, "Error while parsing argument %d\n", i+3 ) ;
+        return 1 ;
+      }
  
-       pattern[i] = (char *)malloc( (l+1) * sizeof( char ) ) ;
-       if ( pattern[i] == NULL ) 
-       {
-           fprintf( stderr, "Unable to allocate string of size %d\n", l ) ;
-           return 1 ;
-       }
-       strncpy( pattern[i], argv[i+3], (l+1) ) ;
-   }
- 
-   /* Allocate the array of matches */
-   n_matches = (int *)malloc( nb_patterns * sizeof( int ) ) ;
-   glob_matches = (int *)malloc( nb_patterns * sizeof( int ) ) ;
-   if ( n_matches == NULL || glob_matches == NULL)
-   {
-       fprintf( stderr, "Error: unable to allocate memory for %ldB\n",
-               nb_patterns * sizeof( int ) ) ;
-       return 1 ;
-   }
- 
- 
- 
-   // Reading and distributing the file by Root
-   if( rank == 0 ){
-     char * buf ;
-     printf( "Approximate Pattern Mathing: "
-             "looking for %d pattern(s) in file %s w/ distance of %d\n", 
-             nb_patterns, filename, approx_factor ) ;
- 
-     buf = read_input_file( filename, &n_bytes ) ;
-     if ( buf == NULL ) return 1 ;
-     
-     buf_size = n_bytes / size;
-     //if (n_bytes % size ) buf_size++; Why ??
- 
-     // Sending size of the local_buf to each Proc
-     for (int to = 1; to < size; to++)
-       MPI_Send(&n_bytes,1, MPI_INT, to, 0, MPI_COMM_WORLD);
-     
-     local_buf_size = n_bytes - buf_size*( size - 1);
-     local_buf = (char *)malloc(sizeof(char)*(local_buf_size));
-     strncpy(local_buf, &( buf[ buf_size*( size - 1) ] ), local_buf_size );
-     
-     // Sending the part of the data to each Proc
-     for (int to = 1; to < size; to++){
-         int start = (to-1)*buf_size;
-         int end = MIN(to*buf_size + max_pat - 1, n_bytes);
-         MPI_Send(&buf[start], end - start, MPI_CHAR, to, 1, MPI_COMM_WORLD);
-     }
-     buf_size = local_buf_size; //only change after data is sent
-     free(buf);
-   }
-   // The rest should receive his part of data
-   else{
-     MPI_Recv(&n_bytes, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
-     buf_size = n_bytes / size;
-     int start = (rank-1)*buf_size;
-     int end = MIN(rank*buf_size + max_pat - 1, n_bytes);
-     local_buf_size = end - start;
-     local_buf = (char *)malloc(sizeof(char)*local_buf_size);
-     MPI_Recv(local_buf, local_buf_size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, NULL);
-   }
+      pattern[i] = (char *)malloc( (l+1) * sizeof( char ) ) ;
+      if ( pattern[i] == NULL ){
+        fprintf( stderr, "Unable to allocate string of size %d\n", l ) ;
+        return 1 ;
+      }
 
-   //Send data to gpu
-    char * cbuf;
-    cbuf = cuda_malloc_cp(local_buf, local_buf_size* sizeof(char));
-    int nblock = 90;
-    int nth_b = 1000;
-    int nth = nblock*nth_b;
-    int * results_th;
-    results_th = cuda_malloc(nth* sizeof(int));
+      strncpy( pattern[i], argv[i+3], (l+1) ) ;
+  }
+ 
+  /* Allocate the array of matches */
+  n_matches = (int *)malloc( nb_patterns * sizeof( int ) ) ;
+  glob_matches = (int *)malloc( nb_patterns * sizeof( int ) ) ;
 
+  if ( n_matches == NULL || glob_matches == NULL){
+    fprintf( stderr, "Error: unable to allocate memory for %ldB\n",
+            nb_patterns * sizeof( int ) ) ;
+   return 1 ;
+  }
+ 
+  // Reading and distributing the file by Root
+  if( rank == 0 ){
+    char * buf ;
+    printf( "Approximate Pattern Mathing: "
+            "looking for %d pattern(s) in file %s w/ distance of %d\n", 
+            nb_patterns, filename, approx_factor ) ;
+ 
+    buf = read_input_file( filename, &n_bytes ) ;
+    if ( buf == NULL ) return 1 ;
     
-   /*****
-    * BEGIN MAIN LOOP
-    ******/
+    // Start of the transfer of the data
+    gettimeofday(&t0, NULL);
+    buf_size = n_bytes / size;
+    // Sending size of the local_buf to each Proc
+    for (int to = 1; to < size; to++)
+      MPI_Send(&n_bytes,1, MPI_INT, to, 0, MPI_COMM_WORLD);
+    
+    local_buf_size = n_bytes - buf_size*( size - 1);
+    local_buf = (char *)malloc(sizeof(char)*(local_buf_size));
+    strncpy(local_buf, &( buf[ buf_size*( size - 1) ] ), local_buf_size );
+    
+    // Sending the part of the data to each Proc
+    for (int to = 1; to < size; to++){
+      int start = (to-1)*buf_size;
+      int end = MIN(to*buf_size + max_pat - 1, n_bytes);
+      MPI_Send(&buf[start], end - start, MPI_CHAR, to, 1, MPI_COMM_WORLD);
+    }
+    
+    buf_size = local_buf_size; //only change after data is sent
+    free(buf);
+  }
+   // The rest should receive his part of data
+  else{
+    MPI_Recv(&n_bytes, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
+    buf_size = n_bytes / size;
+    int start = (rank-1)*buf_size;
+    int end = MIN(rank*buf_size + max_pat - 1, n_bytes);
+    local_buf_size = end - start;
+    local_buf = (char *)malloc(sizeof(char)*local_buf_size);
+    MPI_Recv(local_buf, local_buf_size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, NULL);
+  }
+
+  // Send data to gpu
+  char * cbuf;
+  AssignDevices(rank);
+  cbuf = cuda_malloc_cp(local_buf, local_buf_size* sizeof(char));
+  int nblock = 90;
+  int nth_b = 1000;
+  int nth = nblock*nth_b;
+  int * results_th;
+  results_th = cuda_malloc(nth* sizeof(int));
+
+  /*************************** BEGIN MAIN LOOP ***************************************/
  
-   /* Timer start */
-   if (rank == 0 ) gettimeofday(&t1, NULL);
+  /* Calculation is starting */
+  if (rank == 0 ) gettimeofday(&t1, NULL);
  
    /* Check each pattern one by one */
-   for ( i = 0 ; i < nb_patterns ; i++ )
-   {
-         int size_pattern = strlen(pattern[i]) ;
-       
-       /* Initialize the number of matches to 0 */
-         n_matches[i] = 0 ;
-         int num_matches = 0;
+  for ( i = 0 ; i < nb_patterns ; i++ ){
 
-         //Send pattern to GPU
-        char * cpattern;
-         cpattern =  cuda_malloc_cp(pattern[i],size_pattern* sizeof(char));
+    /* Initialize the number of matches to 0 */
+    n_matches[i] = 0 ;
+  
+    int size_pattern = strlen(pattern[i]) ;
+    int num_matches_cuda;
+    char * cpattern;
+    int num_matches = 0;
+    int cuda_end = 1*buf_size/2;
+    
+    //Send pattern to GPU
+    cpattern =  cuda_malloc_cp(pattern[i],size_pattern* sizeof(char));
+    kernelCall(cpattern,cbuf,cuda_end,local_buf_size,size_pattern,approx_factor,results_th, nth_b,nblock);
+    
+    #pragma omp parallel
+    {
+      //  printf("statt, rank %d\n", rank);
+      int * column = (int *)malloc( (size_pattern+1) * sizeof( int ) ) ;
 
-        int cuda_start = 0;
-        int cuda_end = 2*buf_size/3;
-        kernelCall(cpattern,cbuf,cuda_end,local_buf_size,size_pattern,approx_factor,results_th, nth_b,nblock);
-        int num_matches_cuda;
+      if ( column == NULL ) {
+        fprintf( stderr, "Error: unable to allocate memory for column (%ldB)\n",
+              (size_pattern+1) * sizeof( int ) ) ;
+        exit( EXIT_FAILURE ) ;
+      } // End if
 
-         #pragma omp parallel
-       {
-             //  printf("statt, rank %d\n", rank);
-             int * column = (int *)malloc( (size_pattern+1) * sizeof( int ) ) ;
-             if ( column == NULL ) {
-             fprintf( stderr, "Error: unable to allocate memory for column (%ldB)\n",
-                     (size_pattern+1) * sizeof( int ) ) ;
-             exit( EXIT_FAILURE ) ;
-             } // End if
-             #pragma omp for reduction(+: num_matches) schedule(guided)
-             for ( j = cuda_end ; j < buf_size; j++ ) 
-             {
-                 int distance = 0 ;
-                 int size_pat ;
+      #pragma omp for reduction(+: num_matches) schedule(guided)
+      for ( j = cuda_end ; j < buf_size; j++ ){
+        int distance = 0 ;
+        int size_pat ;
  
-         #if APM_DEBUG
-                 if ( j % 100 == 0 )
-                 {
-                 printf( "Procesing byte %d (out of %d)\n", j, n_bytes ) ;
-                 }
-         #endif
+        size_pat = size_pattern ;
+        if ( local_buf_size < j + size_pattern )
+          size_pat = local_buf_size - j ;
  
-                 size_pat = size_pattern ;
-                 if ( local_buf_size < j + size_pattern )
-                 {
-                     size_pat = local_buf_size - j ;
-                 }
+        distance = levenshtein( pattern[i], &local_buf[j], size_pat, column ) ;
  
-                 distance = levenshtein( pattern[i], &local_buf[j], size_pat, column ) ;
- 
-                 if ( distance <= approx_factor ) {
-                     num_matches++;
-                     
-                 }
-             }// end for loop
-         free( column );
-         #pragma omp single nowait
-               num_matches_cuda =  finalcudaCall(cpattern,cbuf,cuda_end,results_th, nth_b,nblock);
-       }//end pragma omp
+        if ( distance <= approx_factor ) 
+          num_matches++;
 
-        n_matches[i] = num_matches + num_matches_cuda;
-   }
-     MPI_Reduce(n_matches, glob_matches, nb_patterns, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-   /* Timer stop */
-   
-   if( rank == 0 ){
-     gettimeofday(&t2, NULL);
-   
-     duration = (t2.tv_sec -t1.tv_sec)+((t2.tv_usec-t1.tv_usec)/1e6);
-   
-     printf( "APM done in %lf s\n", duration ) ;
-   
-     /*****
-      * END MAIN LOOP
-      ******/
-   
-     for ( i = 0 ; i < nb_patterns ; i++ )
-     {
-         printf( "Number of matches for pattern <%s>: %d\n", 
-                 pattern[i], glob_matches[i] ) ;
-     }
-   }
-   MPI_Finalize();
+        #if APM_DEBUG
+          if ( j % 100 == 0 ){
+            printf( "Procesing byte %d (out of %d)\n", j, n_bytes ) ;
+          }
+        #endif
+
+      }// END for j
+      free( column );
+
+      #pragma omp single nowait
+        num_matches_cuda =  finalcudaCall(cpattern,cbuf,cuda_end,results_th, nth_b,nblock);
+
+    }//END pragma omp
+    n_matches[i] = num_matches + num_matches_cuda;
+
+  } // END for i 
+  MPI_Reduce(n_matches, glob_matches, nb_patterns, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+ /***************************** END MAIN LOOP ***************************************/
+
+  if( rank == 0 ){
+  
+    gettimeofday(&t2, NULL);  // Timer Stop  
+    printf( "\nAPM transfer done in %lf s\nAPM calculation done in %lf s\n\n", DIFFTEMPS(t0,t1), DIFFTEMPS(t1,t2) ) ;
+
+    for ( i = 0 ; i < nb_patterns ; i++ )
+      printf( "Number of matches for pattern <%s>: %d\n", pattern[i], glob_matches[i] ) ;
+  } // END if rank
+  
+  // Cleaning the GPU
+  cuda_free( (void *)cbuf );
+  cuda_free( (void *)results_th);
+
+  MPI_Finalize();
  
-   return 0 ;
- }
- 
+  return 0 ;
+}// END main
--- src/apm_ompic.cu
+++ src/apm_ompic.cu
@@ -9,97 +9,124 @@
  #include <fcntl.h>
  #include <unistd.h>
  #include <sys/time.h>
-
-
  #include <cuda_runtime.h>
  #include <cuda.h>
-
+ 
  #define APM_DEBUG 0
-
  #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
  #define MIN(a, b) ((a) < (b) ? (a) : (b) )
-
-int * results_th ;
-
- void CHECK(cudaError_t err){
-    if( err != cudaSuccess) {
-        printf("Error ! %s \n", cudaGetErrorString(err));
-        exit(1);
-    }  
+ 
+ // MACRO for checking cuda functions
+ #define CHECK(x) \
+ do { \
+   if (x != cudaSuccess) { \
+     fprintf(stderr, "%s:%d: ", __func__, __LINE__); \
+     printf("%s\n",cudaGetErrorString(x)); \
+     exit(EXIT_FAILURE); \
+   } \
+ } while (0)
+  
+  // Kernel function for calculation the levenshtein function
+ __global__ void compMatches(char * pattern, char * buf, int cuda_end, int n_bytes, int size_pattern, int approx_factor, int * resultsth) {
+ 
+   int distance = 0;
+   int size;
+   int i = threadIdx.x + blockIdx.x * blockDim.x;
+   int n_th = gridDim.x * blockDim.x;
+   resultsth[i] = 0;
+   //  if (i == 0) printf("cuda started \n");
+   if (i < cuda_end) {
+     int * column = (int * ) malloc((size_pattern + 1) * sizeof(int));
+     for (int j = i; j < cuda_end; j += n_th) {
+       size = size_pattern;
+       if (n_bytes - j < size_pattern) 
+         size = n_bytes - j;
+ 
+       /////////////////// Levenshtein ///////////////////////////        
+       int len = size;
+       unsigned int x, y, lastdiag, olddiag;
+ 
+       for (y = 1; y <= len; y++) 
+         column[y] = y;
+ 
+       for (x = 1; x <= len; x++) {
+         column[0] = x;
+         lastdiag = x - 1;
+         for (y = 1; y <= len; y++) {
+           olddiag = column[y];
+           column[y] = MIN3(
+             column[y] + 1,
+             column[y - 1] + 1,
+             lastdiag + (pattern[y - 1] == buf[j + x - 1] ? 0 : 1)
+           );
+           lastdiag = olddiag;
+ 
+         }// End for y
+       }// End for x
+       distance = column[len];
+       /////////////// End of Levenshtein ///////////////////////
+ 
+       if(distance <= approx_factor)
+         resultsth[i]++;
+       
+     }// END for j
+   }// END if i 
+ }// END compMatches func.
+ 
+ // Function for calling cuda kernel function
+ extern "C"
+ void kernelCall(char * cpattern, char * cbuf, int cuda_end, int n_bytes, int size_pattern, int approx_factor, int * results_th, int nth_b, int nblock) {
+   compMatches << < nblock, nth_b >>> (cpattern, cbuf, cuda_end, n_bytes, size_pattern, approx_factor, results_th);
+   CHECK(cudaGetLastError());
  }
-
-__global__ void compMatches(char* pattern,char * buf,int cuda_end ,int n_bytes, int size_pattern, int approx_factor, int * resultsth){
-   
-    int distance = 0 ;
-     int size ;
-     int i = threadIdx.x + blockIdx.x*blockDim.x;
-     int n_th = gridDim.x*blockDim.x;
-     resultsth[i] = 0;
-     if(i == 0)     printf("cuda started \n");
-       if(i < cuda_end){
-        int * column = (int *)malloc( (size_pattern+1) * sizeof( int ) ) ;
-           for(int j = i; j < cuda_end; j += n_th){
-               size = size_pattern ;
-               if ( n_bytes - j < size_pattern )
-               {
-                   size = n_bytes - j ;
-               }
-               int len = size;
-               unsigned int x, y, lastdiag, olddiag;
-
-               for (y = 1; y <= len; y++)
-               {
-                   column[y] = y;
-               }
-               for (x = 1; x <= len; x++) {
-                   column[0] = x;
-                   lastdiag = x-1 ;
-                   for (y = 1; y <= len; y++) {
-                       olddiag = column[y];
-                       column[y] = MIN3(
-                               column[y] + 1, 
-                               column[y-1] + 1, 
-                               lastdiag + (pattern[y-1] == buf[j+x-1] ? 0 : 1)
-                               );
-                       lastdiag = olddiag;
-
-                   }
-               }
-               distance = column[len];
-               if ( distance <= approx_factor ) {
-                   resultsth[i]++ ;
-               }
-         }
-       }
-}
-extern "C" void kernelCall(char* cpattern,char * cbuf,int cuda_end ,int n_bytes, int size_pattern, int approx_factor, int * results_th , int  nth_b,int nblock){
-
-    compMatches<<<nblock, nth_b>>>(cpattern,cbuf,cuda_end,n_bytes,size_pattern,approx_factor,results_th);
-    CHECK(cudaGetLastError());
-}
-extern "C" int  finalcudaCall(char* cpattern,char * cbuf, int cuda_end, int * results_th , int  nth_b,int nblock){
-    int * results;
-    int nth = nth_b*nblock;
-    results = (int *)malloc(nth* sizeof(int));
-    CHECK(cudaDeviceSynchronize());
-   printf("cuda done\n");
-    CHECK(cudaMemcpy(results,results_th, nth* sizeof(int), cudaMemcpyDeviceToHost));
-
-    int res = 0;
-    for(int j = 0; j < nth && j < cuda_end; j++){
-        res += results[j];
-    }
-    return res;
-}
-
- extern "C" char * cuda_malloc_cp(char *buf, int size){
-    char *dBuf;
-    CHECK(cudaMalloc( (void**)&dBuf, size));
-    CHECK(cudaMemcpy(dBuf, buf, size, cudaMemcpyHostToDevice) );
-    return dBuf;
-}
-extern "C" int * cuda_malloc(int size){
-    int *dBuf;
-    CHECK(cudaMalloc( (void **)&dBuf, size));
-    return dBuf;
-}
+ 
+ // Function for Synchronizing cuda kernel function and freeing the buffers
+ extern "C"
+ int finalcudaCall(char * cpattern, char * cbuf, int cuda_end, int * results_th, int nth_b, int nblock) {
+   int * results;
+   int nth = nth_b * nblock;
+   results = (int * ) malloc(nth * sizeof(int));
+   CHECK(cudaDeviceSynchronize());
+   //  printf("cuda done\n");
+   CHECK(cudaMemcpy(results, results_th, nth * sizeof(int), cudaMemcpyDeviceToHost));
+ 
+   int res = 0;
+   for (int j = 0; j < nth && j < cuda_end; j++) {
+     res += results[j];
+   }
+   free(results);
+   CHECK(cudaFree(cpattern));
+ 
+   return res;
+ }
+ 
+ // Function for making cudaMalloc and cudaMemcpy from host
+ extern "C"
+ char * cuda_malloc_cp(char * buf, int size) {
+   char * dBuf;
+   CHECK(cudaMalloc((void ** ) & dBuf, size));
+   CHECK(cudaMemcpy(dBuf, buf, size, cudaMemcpyHostToDevice));
+   return dBuf;
+ }
+ 
+ // Function for making cudaMalloc from host
+ extern "C"
+ int * cuda_malloc(int size) {
+   int * dBuf;
+   CHECK(cudaMalloc((void ** ) & dBuf, size));
+   return dBuf;
+ }
+ 
+ extern "C"
+ void cuda_free(void * buf) {
+   CHECK(cudaFree(buf));
+ }
+ 
+ // Function for assigning the proccess to Gpu
+ extern "C"
+ void AssignDevices(int rank) {
+   int nbGPU = 0;
+   CHECK(cudaGetDeviceCount( & nbGPU));
+   CHECK(cudaSetDevice(rank % nbGPU));
+ }
+ 
